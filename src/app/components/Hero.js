6092c1d2b139c4b1f3f30d459465caf20c3d6123
FaresAhmed23 <153539187+FaresAhmed23@users.noreply.github.com>
2024-12-13 14:16:58 +0200

    Update Hero.js

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -67,7 +67,7 @@ installs security systems
 							className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter"
 						>
 							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum has been the industry's standard dummy text
+							industry. Lorem Ipsum has been the industry&apos;s standard dummy text
 							ever since the 1500s.
 						</motion.p>
 
